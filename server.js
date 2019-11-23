1a4fadd6527a8baa931ad55c33ca99dfd56c133d
abdielantonioxd <abdielantonio.af@gmail.com>
2019-11-22 20:57:07 -0500

    conectors

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const path = require("path");
 // Register the connectors here!
 const mongodb_connect = require("./connectors/mongodb/mongodb");
 plugdo.registerConnector("db", "mongodb", mongodb_connect.mongodb());
-plugdo.start(80, path.resolve(__dirname));
+plugdo.start(4000, path.resolve(__dirname));
